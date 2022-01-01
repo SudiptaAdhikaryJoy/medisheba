80aaa6c0072079ef0e83158ccc3c9afa4fe96a03
DaEagle01 <fbinkarim01@gmail.com>
2022-01-01 22:39:52 +0600

    hero created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Pages/Homepage/Home/Home";
 import Footer from "./components/Shared/Footer/Footer";
 import Header from "./components/Shared/Header/Header";
 
@@ -8,7 +9,12 @@ function App() {
       <BrowserRouter>
         <Header></Header>
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
         </Switch>
         <Footer></Footer>
       </BrowserRouter>
