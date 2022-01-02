6c4989f8eb94b57ef26460c538029437f593264b
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-01-02 15:19:02 +0600

    login system update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Login from "./components/Pages/Account/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
