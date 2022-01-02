b5b469815772ccb1ebb5636899e303cf295bc8b5
Suplob <parbaroy30@gmail.com>
2022-01-02 22:51:33 +0600

    load more functionality done

--- src/components/Pages/Homepage/Medicines/Medicines.js
+++ src/components/Pages/Homepage/Medicines/Medicines.js
@@ -7,6 +7,7 @@ import "../../../../App.css";
 const Medicines = () => {
   const [medicines, setMedicines] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
+  const [loadText, setLoadText] = React.useState("Load More");
 
   React.useEffect(() => {
     setLoading(true);
@@ -18,24 +19,35 @@ const Medicines = () => {
       });
   }, []);
 
-  const loadRestMedicines = () => {};
+  const loadRestMedicines = () => {
+    setLoadText("Loading....");
+    axios
+      .get("https://medi-sheba-backend.herokuapp.com/medicines")
+      .then((res) => setMedicines(res.data));
+  };
 
   return (
     <div className="bg-slate-50">
       <div className="container mx-auto py-5">
-        <h1 className="text-5xl my-10">Medcines</h1>
+        <h1 className="text-5xl my-10">Medicines</h1>
         {loading ? (
           <Preloader></Preloader>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-            {medicines.map((data) => (
-              <Card key={data._id} data={data}></Card>
-            ))}
-          </div>
+          <>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+              {medicines.map((data) => (
+                <Card key={data._id} data={data}></Card>
+              ))}
+            </div>
+            {medicines.length <= 6 && (
+              <div className="flex justify-center py-5">
+                <button className="regular-btn" onClick={loadRestMedicines}>
+                  {loadText}
+                </button>
+              </div>
+            )}
+          </>
         )}
-      </div>
-      <div className="flex justify-center pb-5">
-        <button className="regular-btn">Load More</button>
       </div>
     </div>
   );
