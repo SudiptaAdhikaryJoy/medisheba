90ecd98da9f21fded7bf81809ad99cdc033560ac
DaEagle01 <fbinkarim01@gmail.com>
2022-01-02 17:30:12 +0600

    added cart and orders option in header

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -7,71 +7,85 @@ const Header = () => {
 
   return (
     <div>
-      <nav style={{}} className="bg-cyan-300 text-gray-800">
+      <nav style={{}} className="bg-cyan-300 text-gray-800 text-lg">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-start h-16">
-            <div className="flex-shrink-0 grow">
+          <div className="flex items-center justify-start h-20">
+            <div className="flex-shrink-0  my-">
               <Link to="/">
                 {" "}
                 <img
-                  src="https://i.ibb.co/JRs7R6k/green-eco-logo-icon-126523-706-removebg-preview.png"
+                  src="https://i.ibb.co/fdsnM5b/Screenshot-800-removebg-preview.png"
                   alt=""
-                  className="h-24 w-32"
+                  className=" w-64"
                 />
               </Link>
             </div>{" "}
-            <div className="flex flex-shrink-0 grow items-start justify-start">
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-center justify-center space-x-4">
-                  <div></div>
+            <div className="flex flex-shrink-0 grow items-start justify-between">
+              <div className="hidden md:block flex md:flex-row flex-col grow">
+                <div className="ml flex items-center justify-between space-x-4">
                   <div>
                     <Link
                       to="/"
-                      className=" hover:bg-blue-300  text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                      className=" hover:bg-blue-300  text-gray-800 text-lg text-lg px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Home
                     </Link>
-
                     <Link
                       to="/solutions"
-                      className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Solutions
                     </Link>
-
                     <Link
                       to="/blog"
-                      className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Blog
                     </Link>
                     <Link
                       to=""
-                      className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                     >
                       About
                     </Link>
-
                     <Link
                       to="/"
-                      className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Contact
                     </Link>
-
                     <Link
                       to="/"
                       // onClick={handleLogOut}
-                      className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Logout
                     </Link>
-
                     <Link
                       to="/login"
-                      className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                      className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                     >
                       Login
+                    </Link>
+                  </div>
+                  <div className="">
+                    <Link
+                      to="/"
+                      className="text-gray-800 text-lg hover:bg-blue-300
+                      hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm
+                      font-medium"
+                    >
+                      {" "}
+                      <span>
+                        {" "}
+                        <i class="fas fa-cart-plus"></i>Cart
+                      </span>
+                    </Link>
+                    <Link
+                      to="/"
+                      className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
+                    >
+                      <i class="fas fa-cart-plus"></i> Orders
                     </Link>
                   </div>
                 </div>
@@ -81,7 +95,7 @@ const Header = () => {
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
-                className="bg-blue-200 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover: text-gray-800 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-600 focus:ring-white"
+                className="bg-blue-200 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-800 text-lg hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-blue-600 focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
@@ -141,32 +155,32 @@ const Header = () => {
               >
                 <Link
                   to="/"
-                  className="hover:bg-blue-300  text-gray-800 block px-3 py-2 rounded-md text-base font-medium"
+                  className="hover:bg-blue-300  text-gray-800 text-lg block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Home
                 </Link>
                 <Link
                   to="/solutions"
-                  className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Solututions
                 </Link>
                 <Link
                   to="/"
-                  className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Resume
                 </Link>
 
                 <Link
                   to="/"
-                  className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg block px-3 py-2 rounded-md text-base font-medium"
                 >
                   About
                 </Link>
                 <Link
                   to="/"
-                  className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 block px-3 py-2 rounded-md text-base font-medium"
+                  className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Contact
                 </Link>
@@ -174,14 +188,14 @@ const Header = () => {
                 <Link
                   to="/"
                   // onClick={handleLogOut}
-                  className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Logout
                 </Link>
 
                 <Link
                   to="/login"
-                  className="text-gray-800 hover:bg-blue-300 hover: text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-800 text-lg hover:bg-blue-300 hover:text-gray-800 text-lg px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Login
                 </Link>
