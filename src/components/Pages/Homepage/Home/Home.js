cd9ddfb4dbf2c70303aa9f1810bc8a8c771916fd
DaEagle01 <fbinkarim01@gmail.com>
2022-01-03 14:22:41 +0600

    added testimonial section with dynamic star rating. used react-rating for dynamic star rating

--- src/components/Pages/Homepage/Home/Home.js
+++ src/components/Pages/Homepage/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Hero/Hero";
 import Services from "../Services/Services";
+import Testimonial from "../Testimonial/Testimonial";
 
 const Home = () => {
   return (
     <div>
       <Hero></Hero>
       <Services></Services>
+      <Testimonial></Testimonial>
     </div>
   );
 };
