498f308af91414855fec2dcfbe222e301635e818
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2022-01-04 16:54:10 +0600

    conditioning user login

--- src/components/Pages/Homepage/ContactForm/ContactForm.js
+++ src/components/Pages/Homepage/ContactForm/ContactForm.js
@@ -9,7 +9,7 @@ export default function App() {
    
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true, maxLength: 20 })} placeholder="hello" />
+      <input {...register("firstName", { required: true, maxLength: 20 })} placeholder="hello1" />
       <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
       <input type="number" {...register("age", { min: 18, max: 99 })} />
       <input type="submit" />
